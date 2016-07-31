538cfb64601bae853ad925b567aea386a6d78521
Jack Anderson <andersonjackwork@gmail.com>
2016-07-31 17:23:27 +0900

    Updated with best and clever answers

--- 7kyu/Beginner Series #3 Sum of Numbers.js
+++ 7kyu/Beginner Series #3 Sum of Numbers.js
@@ -16,7 +16,7 @@ function GetSum(a, b){
   var low = argArr[0];
   var high = argArr[1];
   var result = 0;
-  
+
   for(var i = low; i <= high; i++){
     result += i;
   }
@@ -24,3 +24,17 @@ function GetSum(a, b){
 }
 
 GetSum(1, 2);
+
+//Best and most clever answer
+const GetSum = (a, b) => {
+  let min = Math.min(a, b),
+      max = Math.max(a, b);
+  return (max - min + 1) * (min + max) / 2;
+}
+
+//Next best answer
+function GetSum( a,b ){
+  if(a == b) return a;
+  else if(a < b) return a + GetSum(a+1, b);
+  else return a + GetSum(a-1,b);
+}
